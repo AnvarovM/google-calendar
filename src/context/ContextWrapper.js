c24f1619b300be9e29a79ac7f7088d2acdfcae25
Anvarovm <anvarovmuxammadxuja2001@gmail.com>
2022-03-06 11:03:07 +0500

    Adde another context

--- src/context/ContextWrapper.js
+++ src/context/ContextWrapper.js
@@ -1,11 +1,20 @@
 import dayjs from 'dayjs';
 import React from 'react';
+import { useEffect } from 'react';
 import GlobalContext from './GlobalContext';
 
 export const ContextWrapper = ({ children }) => {
   const [monthIndex, setMonthIndex] = React.useState(dayjs().month());
+  const [smallCalendarMonth, setSmallCalendarMonth] = React.useState(null);
+  const [daySelected, setDaySelected] = React.useState(null);
+
+  useEffect(() => {
+    if (smallCalendarMonth !== null) {
+      setMonthIndex(smallCalendarMonth);
+    }
+  }, [smallCalendarMonth])
   return (
-    <GlobalContext.Provider value={{monthIndex, setMonthIndex}}>
+    <GlobalContext.Provider value={{monthIndex, setMonthIndex, smallCalendarMonth, setSmallCalendarMonth, daySelected, setDaySelected}}>
         {children}
     </GlobalContext.Provider>
   )
--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -3,6 +3,10 @@ import React from 'react'
 const GlobalContext = React.createContext({
     monthIndex: 0,
     seMonthIndex: (index) => {},
+    smallCalendarMonth: null,
+    setSmallCalendarMonth: (index) => {},
+    daySelected: null,
+    setDaySelected: (day) => {}
 });
 
 export default GlobalContext;
